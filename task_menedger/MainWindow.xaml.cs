03faf34bf45b6baf7e559f24f654e8eaef20331c
232415 <232415@DESKTOP-10MIQUT>
2026-06-15 16:50:04 +0300

    ksbvkbv

--- task_menedger/MainWindow.xaml.cs
+++ task_menedger/MainWindow.xaml.cs
@@ -15,14 +15,58 @@ using System.Windows.Shapes;
 
 namespace task_menedger
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        public class TaskItem
+        {
+            public string Name;
+            public string Priority;
+            public bool IsComlete;
+            public string DisplayText => $"{(IsComlete ? "[X]" : "[ ]")} {Name} ({Priority})";
+        }
+
+        private List<TaskItem> allTasks = new List<TaskItem>();
+
         public MainWindow()
         {
             InitializeComponent();
+            allTasks.Add(new TaskItem {Name = "Купить продукты", Priority = "Высокий", IsComlete = true});
+            allTasks.Add(new TaskItem { Name = "Позванить маме", Priority = "Высокий", IsComlete = false});
+            allTasks.Add(new TaskItem { Name = "Сделать домашнюю работу", Priority = "Низкий", IsComlete = true});
+        }
+
+        private void AddBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string taskName = TaskNameTB.Text.Trim();
+            
+            if (string.IsNullOrWhiteSpace(taskName))
+            {
+                MessageBox.Show("Введите название задания", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            ComboBoxItem selectedPriority = PriorityCmb.SelectedItem as ComboBoxItem;
+            string priority = selectedPriority.Content.ToString();
+        }
+
+        private void CompleteBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DeleteBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SortBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void FilterAllRbt_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
